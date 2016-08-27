740bee0d8f618b39fefff73b0649ebbcadd4d584
Benjamin Werden <doublemintben@gmail.com>
2016-08-27 13:50:44 -0400

    added health pickup

--- GameyMickGameFace/GameObjects/PowerUp.cs
+++ GameyMickGameFace/GameObjects/PowerUp.cs
@@ -1,5 +1,7 @@
-﻿using GameyMickGameFace.Physics;
+﻿using GameyMickGameFace.Media;
+using GameyMickGameFace.Physics;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
@@ -13,23 +15,26 @@ namespace GameyMickGameFace.GameObjects
         public string Name { get; set; }
         public Texture2D Image { get; set; }
         public Body PhysicsBody { get; set; }
+        Audio se;
 
         public PowerUp()
         {
-            PhysicsBody = new Body(new Point(100, 100), 95, 86, 0, 100, .85f);
+            PhysicsBody = new Body(new Point(200, 200), 95, 86, 0, 100, .85f, this);
+            se = new Audio();
         }
 
         public void OnPickup(Player effectedPlayer)
         {
-
+            //do something
+            Media.Audio.damage.Play();
         }
 
-        public void draw(GameTime time, SpriteBatch batch)
+        public void Draw(GameTime time, SpriteBatch batch)
         {
-
+            batch.Draw(Image, PhysicsBody.Position, Color.White);
         }
 
-        public void update(GameTime time)
+        public void Update(GameTime time)
         {
 
         }
