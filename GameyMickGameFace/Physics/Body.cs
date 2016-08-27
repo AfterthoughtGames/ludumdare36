f6d842f6695a58863dd720904c1c879d618ac980
magicrat <magicrat@gmail.com>
2016-08-27 08:14:36 -0400

    jackass

---
+++ GameyMickGameFace/Physics/Body.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameyMickGameFace.Physics
+{
+    public class Body
+    {
+        public Rectangle PhysicsBody;
+        public Vector2 Velocity;
+        public float Restitution;
+        public int Mass;
+        public float InverseMass;
+
+        public Body(Point position, int width, int height, float restitution, int mass)
+        {
+            PhysicsBody = new Rectangle(position.X, position.Y, width, height);
+            Velocity = new Vector2(0, 0);  //init to no veleocity
+            Restitution = restitution;
+            Mass = mass;
+
+            //make sure that if no mass is set the thing cannot be moved
+            if (Mass == 0)
+            {
+                InverseMass = 0;
+            }
+            else
+            {
+                InverseMass = 1 / Mass;
+            }
+        }
+    }
+}
---
+++ GameyMickGameFace/Physics/PhysicsManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameyMickGameFace.Physics
+{
+    public class PhysicsManager
+    {
+        public void ResolveCollision(Body a, Body b)
+        {
+            //find the collision normal (aka the direction of the collision
+            Vector2 collisionNormal = (b.PhysicsBody.Location - a.PhysicsBody.Location).ToVector2();
+
+            // Calculate relative velocity
+            Vector2 relativeVelocity = b.Velocity - a.Velocity;
+
+            // Calculate relative velocity in terms of the normal direction
+            float velAlongNormal = Vector2.Dot(relativeVelocity, collisionNormal);
+
+            // Do not resolve if velocities are separating
+            if (velAlongNormal > 0)
+            {
+                return;
+            }
+
+            // Calculate restitution
+            float restitution = Math.Min(a.Restitution, b.Restitution);
+
+            // Calculate impulse scalar
+            float impulseScalar = -(1 + restitution) * velAlongNormal;
+            impulseScalar /= a.InverseMass + b.InverseMass;
+
+            // Apply impulse
+            Vector2 impulse = impulseScalar * collisionNormal;
+            a.Velocity -= a.InverseMass * impulse;
+            b.Velocity += b.InverseMass * impulse;
+        }
+    }
+}
