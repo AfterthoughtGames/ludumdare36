649838bfc626c8c340c18cebeab340967fffb027
Brian Krygsman <bkrygsman@ostusa.com>
2016-08-27 16:40:36 -0400

    Add smoke frames and Smoke animation

--- GameyMickGameFace/Media/Animations.cs
+++ GameyMickGameFace/Media/Animations.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GameyMickGameFace.Media
 {
@@ -14,6 +9,7 @@ namespace GameyMickGameFace.Media
         {
              PlayerWalk = new Animation(50);
              PlayerIdel = new Animation(10);
+            PotionSmoke = new Animation( 50 );
 
 
              PlayerWalk.AddTexture(cm.Load<Texture2D>("Otter_Otter_Walking_000"));
@@ -77,9 +73,27 @@ namespace GameyMickGameFace.Media
              PlayerIdel.AddTexture(cm.Load<Texture2D>("Otter_Otter_Idle_027"));
              PlayerIdel.AddTexture(cm.Load<Texture2D>("Otter_Otter_Idle_028"));
              PlayerIdel.AddTexture(cm.Load<Texture2D>("Otter_Otter_Idle_029"));
+
+            PotionSmoke.AddTexture( cm.Load<Texture2D>( "Animations/Smoke_15/0000" ) );
+            PotionSmoke.AddTexture( cm.Load<Texture2D>( "Animations/Smoke_15/0006" ) );
+            PotionSmoke.AddTexture( cm.Load<Texture2D>( "Animations/Smoke_15/0012" ) );
+            PotionSmoke.AddTexture( cm.Load<Texture2D>( "Animations/Smoke_15/0018" ) );
+            PotionSmoke.AddTexture( cm.Load<Texture2D>( "Animations/Smoke_15/0024" ) );
+            PotionSmoke.AddTexture( cm.Load<Texture2D>( "Animations/Smoke_15/0030" ) );
+            PotionSmoke.AddTexture( cm.Load<Texture2D>( "Animations/Smoke_15/0036" ) );
+            PotionSmoke.AddTexture( cm.Load<Texture2D>( "Animations/Smoke_15/0042" ) );
+            PotionSmoke.AddTexture( cm.Load<Texture2D>( "Animations/Smoke_15/0048" ) );
+            PotionSmoke.AddTexture( cm.Load<Texture2D>( "Animations/Smoke_15/0054" ) );
+            PotionSmoke.AddTexture( cm.Load<Texture2D>( "Animations/Smoke_15/0060" ) );
+            PotionSmoke.AddTexture( cm.Load<Texture2D>( "Animations/Smoke_15/0066" ) );
+            PotionSmoke.AddTexture( cm.Load<Texture2D>( "Animations/Smoke_15/0072" ) );
+            PotionSmoke.AddTexture( cm.Load<Texture2D>( "Animations/Smoke_15/0078" ) );
+            PotionSmoke.AddTexture( cm.Load<Texture2D>( "Animations/Smoke_15/0084" ) );
+            PotionSmoke.AddTexture( cm.Load<Texture2D>( "Animations/Smoke_15/0090" ) );
         }
 
         public static Animation PlayerWalk { get; set; }
         public static Animation PlayerIdel { get; set; }
+        public static Animation PotionSmoke { get; set; }
     }
 }
